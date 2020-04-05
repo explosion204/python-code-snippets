5edfa1a5b07237f0048c09007a022f3354932392
explosion204 <dzmitriy20magic@gmail.com>
2020-04-05 21:58:00 +0300

    external merge sort & singleton metaclass

---
+++ singleton_metaclass.py
@@ -0,0 +1,6 @@
+class SingletonMeta(type):
+    def __call__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(SingletonMeta, cls).__call__(*args, **kwargs)
+
+        return cls.instance
